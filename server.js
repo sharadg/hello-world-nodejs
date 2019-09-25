bc81d68be922aeee39d1efe3aa0e746cbcdc39a1
sharadg <91043+sharadg@users.noreply.github.com>
2019-09-25 15:16:45 -0500

    HCA demo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
     console.log('Received request for URL: ' + request.url);
     response.writeHead(200);
-    response.end('Hello Warming World!');
+    response.end('<html><head><title>PKS Demo</title></head><body><img src="https://hcahealthcare.com/contentAsset/raw-data/f430917e-9fa1-4f1a-8dc7-d652c0b00e71/fullColorLogo" width=50% align=center /></body></html>');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
